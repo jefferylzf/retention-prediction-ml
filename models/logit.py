6f8f1019964d762d843cc42a7a39731f1ae24760
liuzhuofan <jefferylzf@outlook.com>
2020-09-29 12:11:04 +0800

    upload

--- models/logit.py
+++ models/logit.py
@@ -1,1 +1,6 @@
 import sklearn
+import pandas as pd
+
+
+param = {'data-name':'churaned_dataset_0902'}
+csv_file = pd.read_csv("../raw-data/%s".format(param['data-name']), header = 0)
--- preprocess/data-load.py
+++ preprocess/data-load.py
@@ -1,6 +1,31 @@
 import numpy as np
 import pandas as pd
-churned_user = pd.read_csv("../raw-data/churned_dataset_0902.csv", header = 0)
-churned_user['label'] = 0
-churned_user.to_csv('../raw-data/churned_user_0902.csv')
+
+param = {'input-data-name':'churned_dataset_0902.csv',
+         'output-data-name':'churned_users_0902.csv',
+         'bool_retained': 0
+         }
+
+
+csv_file = pd.read_csv(("../raw-data/{}").format(param['input-data-name']), header = 0)
+csv_file['pvp-unlock'] = 0
+csv_file['hunt-unlock'] = 0
+csv_file['exp-unlock'] = 0
+csv_file['label'] = param['bool_retained']
+
+for idx in range(len(csv_file)):
+    lv = csv_file.loc[idx, 'level_cnt']
+    if lv >= 70:
+        csv_file.loc[idx, 'pvp-unlock'] = 1
+        csv_file.loc[idx, 'hunt-unlock'] = 1
+        csv_file.loc[idx, 'exp-unlock'] = 1
+    if lv >= 30:
+        csv_file.loc[idx, 'hunt-unlock'] = 1
+        csv_file.loc[idx, 'pvp-unlock'] = 1
+    if lv >= 15:
+        csv_file.loc[idx, 'hunt-unlock'] = 1
+
+csv_file.to_csv(('../raw-data/{}').format(param['output-data-name']))
+
+
 print('finally')
